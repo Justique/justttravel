28c7e5f54385ff65f6db92f5d1b1d8505ed02ae9
Efexis <efexlike@gmail.com>
2017-04-26 14:27:40 +0300

    Фикс сообщения после отправки формы beta версии

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,27 +1,25 @@
 <?php
-/* @var $this \yii\web\View */
-use yii\helpers\ArrayHelper;
 use yii\widgets\Breadcrumbs;
 
+/* @var $this \yii\web\View */
 /* @var $content string */
 
 $this->beginContent('@frontend/views/layouts/base.php')
 ?>
-        <div class="wrapper">
-        <?php echo Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
+<div class="wrapper">
+    <?php echo Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
 
-        <?php if(Yii::$app->session->hasFlash('alert')):?>
-            
-            <?php 
-            
-            $alert = Yii::$app->session->getFlash('alert');
-            
-            $this->registerJs("
-                swal('Успешно', '" . $alert['body'] . "', 'success');
-            "); ?>
-        <?php endif; ?>
-        </div>
-        <?php echo $content ?>
+    <?php if(Yii::$app->session->hasFlash('alert')):?>
+        <?php
+        $alert = Yii::$app->session->getFlash('alert');
+        $this->registerJs("
+            swal('Успешно', '" . $alert . "', 'success');
+        "); ?>
+    <?php endif; ?>
+</div>
+
+<?php echo $content ?>
+
 <?php $this->endContent() ?>
