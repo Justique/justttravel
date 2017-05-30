010c47a73903ebe2f42845217ab84fbcf7038f6f
Efexis <efexlike@gmail.com>
2017-05-31 02:17:21 +0300

    Шаблон избранных туров

--- frontend/modules/user/views/toursfavorites/index.php
+++ frontend/modules/user/views/toursfavorites/index.php
@@ -11,17 +11,19 @@ use yii\widgets\ListView;
 $this->title = 'Tours Favorits';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="content-wrapper with-counter">
-    <h1>Все визы<span class="country-count"><?php echo $dataProvider->getTotalCount(); ?></span></h1>
-    <p>Визы всех турфирм из нашего каталога</p>
-    <ul class="tours-list">
-        <?= ListView::widget([
-            'dataProvider' => $dataProvider,
-            'options' => [
-                'class' => ''
-            ],
-            'itemOptions' => ['class' => 'tours-list'],
-            'itemView' => '_item',
-        ]) ?>
-    </ul>
-</div>
+<section class="visa-page">
+    <div class="content-wrapper with-counter">
+        <h1>Все туры<span class="country-count"><?php echo $dataProvider->getTotalCount(); ?></span></h1>
+        <p>Туры всех турфирм из нашего каталога</p>
+        <ul class="tours-list">
+            <?= ListView::widget([
+                'dataProvider' => $dataProvider,
+                'options' => [
+                    'class' => ''
+                ],
+                'itemOptions' => ['class' => 'tours-list'],
+                'itemView' => '_item',
+            ]) ?>
+        </ul>
+    </div>
+</section>
