fd55067cd694331eec06c4f9e837d73dd700059a
Efexis <efexlike@gmail.com>
2017-06-02 16:37:12 +0300

    typo

--- frontend/views/layouts/_clear.php
+++ frontend/views/layouts/_clear.php
@@ -30,7 +30,7 @@ use yii\helpers\Html;
 
 
 </body>
-<?php if(YII_DEBUG): ?>
+<?php if(!YII_DEBUG): ?>
     <script type="text/javascript" src="//vk.com/js/api/openapi.js?146"></script>
     <!-- VK Widget -->
     <div id="vk_community_messages"></div>
