57708006c90ea9b08038eadee3653f89b79d6517
Efexis <efexlike@gmail.com>
2017-06-03 14:00:25 +0300

    typo

--- frontend/views/layouts/_clear.php
+++ frontend/views/layouts/_clear.php
@@ -86,7 +86,7 @@ use yii\helpers\Html;
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','../../www.google-analytics.com/analytics.js','ga');
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
         ga('create', 'UA-52206830-1', 'mordorthemes.com');
         ga('send', 'pageview');
