8e15834d3650cccf9ece7b6a2ea20bf62e7af2cd
Efexis <efexlike@gmail.com>
2017-07-19 20:49:49 +0300

    typo

--- frontend/modules/user/views/toursfavorites/index.php
+++ frontend/modules/user/views/toursfavorites/index.php
@@ -12,9 +12,9 @@ $this->title = 'Tours Favorits';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <section class="visa-page">
-    <div class="content-wrapper with-counter">
+    <div class="content-wrapper with-counter" style="padding: 0 3%;">
         <h1>Избранные туры<span class="country-count"><?php echo $dataProvider->getTotalCount(); ?></span></h1>
-        <ul class="tours-list">
+
             <?= ListView::widget([
                 'dataProvider' => $dataProvider,
                 'options' => [
@@ -23,6 +23,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 'itemOptions' => ['class' => 'tours-list'],
                 'itemView' => '_item',
             ]) ?>
-        </ul>
+
     </div>
 </section>
