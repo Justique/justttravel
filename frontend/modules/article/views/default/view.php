fb4c7bde79bace8f22795fd9bc0a4473b5f6d32e
Efexis <efexlike@gmail.com>
2017-07-19 18:39:51 +0300

    Кнопки Pluso

--- frontend/modules/article/views/default/view.php
+++ frontend/modules/article/views/default/view.php
@@ -36,18 +36,17 @@
             <h5><?php echo $model->getCF('title'); ?></h5> -->
             <div><?php echo $model->body; ?></div>
         </div>
-        <script type="text/javascript">
-         (function() {
-                  if (window.pluso)if (typeof window.pluso.start == "function") return;
-                  if (window.ifpluso==undefined) { window.ifpluso = 1;
-                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-                    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-                    var h=d[g]('body')[0];
-                    h.appendChild(s);
-                    }})();
+        <script type="text/javascript">(function() {
+            if (window.pluso)if (typeof window.pluso.start == "function") return;
+            if (window.ifpluso==undefined) { window.ifpluso = 1;
+                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                var h=d[g]('body')[0];
+                h.appendChild(s);
+            }})();
         </script>
-        <div class="pluso" data-background="#ebebeb" data-options="big,square,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print"></div>
+        <div class="pluso" data-background="none;" data-options="small,square,line,horizontal,counter,sepcounter=1,theme=14" data-services="vkontakte,facebook,twitter,google,odnoklassniki,moimir,print"></div>
         <hr class="medium">
 
 
